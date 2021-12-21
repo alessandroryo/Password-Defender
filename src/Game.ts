d00beb60c3f718894ad5b06fe9947ab9b1bc3f03
alessandroryo <anan0002@hz.nl>
2021-12-21 09:54:39 +0100

    Co-authored-by: LifeAdmin-cmd <LifeAdmin-cmd@users.noreply.github.com>

---
+++ src/Game.ts
@@ -0,0 +1,61 @@
+export default class Game {
+  public readonly canvas: HTMLCanvasElement;
+
+  public readonly ctx: CanvasRenderingContext2D;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  /**
+   * Writes text to the canvas
+   *
+   * @param text - Text to write
+   * @param xCoordinate - Horizontal coordinate in pixels
+   * @param yCoordinate - Vertical coordinate in pixels
+   * @param fontSize - Font size in pixels
+   * @param color - The color of the text
+   * @param alignment - Where to align the text
+   */
+  public writeTextToCanvas(
+    text: string,
+    xCoordinate: number,
+    yCoordinate: number,
+    fontSize: number = 20,
+    color: string = 'white',
+    alignment: CanvasTextAlign = 'center',
+  ): void {
+    const ctx = this.canvas.getContext('2d');
+    ctx.font = `${fontSize}px sans-serif`;
+    ctx.fillStyle = color;
+    ctx.textAlign = alignment;
+    ctx.fillText(text, xCoordinate, yCoordinate);
+  }
+
+  /**
+     * Method to load an image
+     *
+     * @param source the source
+     * @returns HTMLImageElement - returns an image
+     */
+  public static loadNewImage(source: string): HTMLImageElement {
+    const img = new Image();
+    img.src = source;
+    return img;
+  }
+
+  /**
+     * Returns a random number between min and max
+     *
+     * @param min - lower boundary
+     * @param max - upper boundary
+     * @returns a random number between min and max
+     */
+  public static randomNumber(min: number, max: number): number {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+}
